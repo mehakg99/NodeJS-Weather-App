bae8f77571bba512c02ceacedf1a9a5d543a7dfb
Mehak <mehakg99@gmail.com>
2021-01-07 02:55:41 +0530

    create app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     message_1.textContent = 'Loading...';
     message_2.textContent = '';
 
-    fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
+    fetch('/weather?address='+search.value).then((response) => {
         response.json().then((data) => {
             if(data.Error){
                 message_1.textContent = data.Error;
